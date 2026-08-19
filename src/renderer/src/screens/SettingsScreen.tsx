3434cb50b7402eefc0fd5f09073da6432f532229
Karm Patel <karm.patel124@gmail.com>
2026-08-19 11:09:48 -0400

    Add Google Drive backup card to Settings

--- src/renderer/src/screens/SettingsScreen.tsx
+++ src/renderer/src/screens/SettingsScreen.tsx
@@ -16,6 +16,7 @@ import { ReportEmailSettingsCard } from '../components/ReportEmailSettingsCard'
 import { ReportCsvExportSettingsCard } from '../components/ReportCsvExportSettingsCard'
 import { UpdateSettingsCard } from '../components/UpdateSettingsCard'
 import { BackupModal } from '../components/BackupModal'
+import { GoogleDriveBackupCard } from '../components/GoogleDriveBackupCard'
 import { RestoreBackupModal } from '../components/RestoreBackupModal'
 import { DeleteAllDataModal } from '../components/DeleteAllDataModal'
 import { Card, CardHeader, CardTitle, CardDescription } from '../components/ui/Card'
@@ -993,6 +994,8 @@ export function SettingsScreen() {
               </div>
             </Card>
           )}
+
+          {sectionVisible('backup') && <GoogleDriveBackupCard />}
         </div>
       )}
 
